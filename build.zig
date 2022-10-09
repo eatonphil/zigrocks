731c44705c92b8585cfda7f5a80e47c9edefdef1
Phil Eaton <phil@eatonphil.com>
2022-10-09 14:59:21 +0200

    set/get commands working

--- build.zig
+++ build.zig
@@ -3,8 +3,15 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("main", "main.zig");
     exe.linkLibC();
-    exe.addIncludeDir("rocksdb/include");
-    exe.addLibPath("rocksdb");
     exe.linkSystemLibraryName("rocksdb");
+    exe.addLibraryPath("./rocksdb");
+    exe.addIncludePath("./rocksdb/include");
+    exe.setOutputDir(".");
+
+    if (exe.target.isDarwin()) {
+        b.installFile("./rocksdb/librocksdb.7.8.dylib", "../librocksdb.7.8.dylib");
+        exe.addRPath(".");
+    }
+        
     exe.install();
 }
--- main.zig
+++ main.zig
@@ -1,13 +1,75 @@
 const std = @import("std");
-const cstd = @cImport(@cInclude("stdlib.h"));
-const c = @cImport(@cInclude("rocksdb/c.h"));
+const rdb = @cImport(@cInclude("rocksdb/c.h"));
+
+pub fn set(db: *rdb.rocksdb_t, key: [:0]const u8, value: [:0]const u8) void {
+    var writeOptions = rdb.rocksdb_writeoptions_create();
+    var err: ?[*:0]u8 = null;
+    rdb.rocksdb_put(db, writeOptions, @ptrCast([*c]const u8, key), key.len, @ptrCast([*c]const u8, value), value.len, &err);
+    if (err) |errStr| {
+        std.debug.print("Could not write key-value pair: {s}", .{errStr});
+        std.c.free(@ptrCast(*anyopaque, err));
+        return;
+    }
+}
+
+const RocksDBError = error {
+    NotFound,
+};
+
+pub fn get(db: *rdb.rocksdb_t, key: [:0]const u8) ![*c]const u8 {
+    var readOptions = rdb.rocksdb_readoptions_create();
+    var valueLength: usize = 0;
+    var err: ?[*:0]u8 = null;
+    var v = rdb.rocksdb_get(db, readOptions, @ptrCast([*c]const u8, key), key.len, &valueLength, &err);
+    if (err) |errStr| {
+        std.debug.print("Could not get value for key: {s}", .{errStr});
+        std.c.free(@ptrCast(*anyopaque, err));
+        return "";
+    }
+    if (v == 0) {
+        return RocksDBError.NotFound;
+    }
+
+    return v;
+}
 
 pub fn main() !void {
-    var options: ?*c.rocksdb_options_t = c.rocksdb_options_create();
-    c.rocksdb_options_set_create_if_missing(options, 1);
-    var err: []u8 = "";
-    _ = c.rocksdb_open(options, "/tmp/testdb", @ptrCast([*c][*c]u8, &err));
-    if (err.len != 0) {
-        std.debug.print("err: {s}", .{err});
+    var options: ?*rdb.rocksdb_options_t = rdb.rocksdb_options_create();
+    rdb.rocksdb_options_set_create_if_missing(options, 1);
+    var err: ?[*:0]u8 = null;
+    var db = rdb.rocksdb_open(options, "/tmp/testdb", &err);
+    if (err) |errStr| {
+        std.debug.print("err: {s}", .{errStr});
+        std.c.free(@ptrCast(*anyopaque, err));
+        return;
+    }
+    defer rdb.rocksdb_close(db);
+
+    var args = std.process.args();
+    var key: [:0]const u8 = "";
+    var value: [:0]const u8 = "";
+    var command = "get";
+    _ = args.next(); // Skip first arg
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "set")) {
+            key = args.next().?;
+            value = args.next().?;
+            command = "set";
+        } else if (std.mem.eql(u8, arg, "get")) {
+            key = args.next().?;
+        } else {
+            std.debug.print("Must specify command (get or set). Got: {s}", .{arg});
+            return;
+        }
+    }
+
+    if (std.mem.eql(u8, command, "set")) {
+        set(db.?, key, value);
+    } else {
+        var v = get(db.?, key) catch {
+            std.debug.print("Key not found.", .{});
+            return;
+        };
+        std.debug.print("{s}", .{v});
     }
 }
